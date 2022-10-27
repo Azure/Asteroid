fcb70544da6af67b863e116e4980a6d7caf35a54
demirsenturk <demirsenturk@yahoo.com>
2022-10-27 02:13:00 +0200

    added image content

--- react-gh/src/App.js
+++ react-gh/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import rocket from './media/rocket.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={rocket} className="App-logo" alt="rocket" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
