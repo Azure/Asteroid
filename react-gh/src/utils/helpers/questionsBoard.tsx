0c06172abfa9e473a2a76c9d3404cc3f8fa8ed54
70mm1 <mail@tmirwald.de>
2022-12-27 19:01:11 +0100

    inclusion of child and grandchild questions

--- react-gh/src/utils/helpers/questionsBoard.tsx
+++ react-gh/src/utils/helpers/questionsBoard.tsx
@@ -1,48 +1,89 @@
 import { Label, Link, Stack } from "@fluentui/react";
-import { IQChoice } from "../../components/QIcheckbox";
-import { QChoice } from "../../components/q_checkbox";
+import { Input, RadioGroup, Radio } from "@fluentui/react-components";
+
 // Local imports
 import data from "../data.json";
 
 export const adv_stackstyle = {
-    root: { border: "1px solid", margin: "10px 0 10px", padding: "15px" },
-  };
+  root: { border: "1px solid", margin: "10px 0 10px", padding: "15px" },
+};
+
+const questionStyle = { paddingLeft: "100px" };
+const radioStyle = { border: "1px solid" };
 
 // Output questions from JSON KnowledgeBase file
 export function categoryQuestions(category: string) {
-
-    var questions:any = {};
-
-    const paramsByCategory = data.map((param) => {
-        if (param.Parent.CategoryID == category)
-          return (
-            <Stack tokens={{ childrenGap: 15 }} style={{ marginTop: 0, marginLeft: "50px" }}>
-                <Stack horizontal tokens={{}}>
-                    <Stack.Item>
-                        <Label style={{}}>
-                        {param.Parent.Question + " "}
-                        <Link
-                            target="_"
-                            href={param.Parent.ExplanationLearnLink}
+  const renderQuestionsByCategory = data.map((parent) => {
+    if (parent.Parent.CategoryID == category) {
+      return (
+        // html div with padding left to indent the question
+        <div className="Parent Question" style={questionStyle}>
+          <Label id="QuestionTitle">{parent.Parent.Question}</Label>
+          {renderAnswers(parent.Parent.AnswerType, parent.Parent.Answers)}
+          {parent.Childs.map((child) => {
+            if (parent.Parent.AnswerChoice == true)
+              return (
+                <div className="Child Question" style={questionStyle}>
+                  <Label id="QuestionTitle">{child.Parent.Question}</Label>
+                  {renderAnswers(child.Parent.AnswerType, child.Parent.Answers)}
+                  {child.Childs.map((grandchild) => {
+                    if (child.Parent.AnswerChoice == true)
+                      return (
+                        <div
+                          className="GrandChild Question"
+                          style={questionStyle}
                         >
-                            docs
-                        </Link>
-                        </Label>
-                        <QChoice text1={String(param.Parent.Answers[0])} text2={String(param.Parent.Answers[1])} />
-                    </Stack.Item>
-                </Stack>
-            </Stack>
-          );
-        return null;
-      });
+                          <Label id="QuestionTitle">
+                            {grandchild.Parent.Question}
+                          </Label>
+                          {renderAnswers(
+                            grandchild.Parent.AnswerType,
+                            grandchild.Parent.Answers
+                          )}
+                        </div>
+                      );
+                  })}
+                </div>
+              );
+          })}
+        </div>
+      );
+    }
+    return null;
+  });
 
-return ( 
-<Stack tokens={{ childrenGap: 15 }} styles={adv_stackstyle}>
-    <Label style={{}}>
-        {category}
-    </Label>
-    {paramsByCategory.filter((c, i) => c != null ).map((c, i) =>
-        <div key={i}>{c}</div>
-    )}
-</Stack>)
-};
+  return (
+    <Stack tokens={{ childrenGap: 15 }} styles={adv_stackstyle}>
+      <Label style={{}}>{category}</Label>
+      {renderQuestionsByCategory
+        .filter((c, i) => c != null)
+        .map((c, i) => (
+          <div key={i}>{c}</div>
+        ))}
+    </Stack>
+  );
+}
+
+function renderAnswers(answerType: string, answers: string[]) {
+  switch (answerType) {
+    case "boolean":
+      return (
+        <RadioGroup aria-labelledby="label835">
+          <Radio label="Yes" value="true" />
+          <Radio label="No" value="false" />
+        </RadioGroup>
+      );
+    case "stringInput":
+      return <Input id="Input0" placeholder={answers[0]} />;
+    case "stringSelection":
+      return (
+        <RadioGroup aria-labelledby="label935">
+          {answers.map((answer) => {
+            return <Radio label={answer} value={answer} />;
+          })}
+        </RadioGroup>
+      );
+    default:
+      break;
+  }
+}
