1b4aefcaaa8fbcc754a15dc63957908a5f493944
70mm1 <mail@tmirwald.de>
2022-12-29 11:15:23 +0100

    Updated Node Modules

--- react-gh/node_modules/deep-equal/test/cmp.js
+++ react-gh/node_modules/deep-equal/test/cmp.js
@@ -847,6 +847,16 @@ test('regexen', function (t) {
     false
   );
 
+  var c = /abc/g;
+  var d = /abc/i;
+  t.deepEqualTest(
+    c,
+    d,
+    'two regexes with the same source but different flags',
+    false,
+    false
+  );
+
   t.end();
 });
 
@@ -1161,6 +1171,67 @@ test('TypedArrays', { skip: !hasTypedArrays }, function (t) {
       false,
       false
     );
+
+    st.end();
+  });
+
+  t.test('ArrayBuffers', { skip: typeof ArrayBuffer !== 'function' }, function (st) {
+    var buffer1 = new ArrayBuffer(8); // initial value of 0's
+    var buffer2 = new ArrayBuffer(8); // initial value of 0's
+
+    var view1 = new Int8Array(buffer1);
+    var view2 = new Int8Array(buffer2);
+
+    st.deepEqualTest(
+      view1,
+      view2,
+      'Int8Arrays of similar ArrayBuffers',
+      true,
+      true
+    );
+
+    st.deepEqualTest(
+      buffer1,
+      buffer2,
+      'similar ArrayBuffers',
+      true,
+      true
+    );
+
+    for (var i = 0; i < view1.byteLength; i += 1) {
+      view1[i] = 9; // change all values to 9's
+    }
+
+    st.deepEqualTest(
+      view1,
+      view2,
+      'Int8Arrays of different ArrayBuffers',
+      false,
+      false
+    );
+
+    st.deepEqualTest(
+      buffer1,
+      buffer2,
+      'different ArrayBuffers',
+      false,
+      false
+    );
+
+    t.test('lies about byteLength', { skip: !('byteLength' in ArrayBuffer.prototype) }, function (s2t) {
+      var empty4 = new ArrayBuffer(4);
+      var empty6 = new ArrayBuffer(6);
+      Object.defineProperty(empty6, 'byteLength', { value: 4 });
+
+      s2t.deepEqualTest(
+        empty4,
+        empty6,
+        'different-length ArrayBuffers, one lying',
+        false,
+        false
+      );
+      s2t.end();
+    });
 
     st.end();
   });
