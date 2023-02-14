9fa3306fbce93d25e32185c8fc782173490a16fe
70mm1 <mail@tmirwald.de>
2023-02-14 17:33:21 +0100

    Encapsulated Chat Component in own function

---
+++ react-gh/src/components/Chat.tsx
@@ -0,0 +1,85 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogActions,
+  DialogSurface,
+  DialogTitle,
+  DialogBody,
+  Label,
+  Input,
+  useId,
+  Text,
+  Subtitle2,
+  Button,
+} from "@fluentui/react-components";
+
+import { Persona, PersonaProps } from "@fluentui/react-components/unstable";
+import * as React from "react";
+
+import { SendRegular } from "@fluentui/react-icons";
+
+export const Chat: React.FunctionComponent = ({}) => {
+  const underlineId = useId("input-underline");
+  return (
+    <Dialog modalType="non-modal">
+      <DialogTrigger disableButtonEnhancement>
+        <Button appearance="transparent">
+          <Persona
+            name="CS·AI Bot"
+            size="huge"
+            secondaryText="Ask Azure Questions"
+            presence={{ status: "available" }}
+            avatar={{
+              image: {
+                src: "https://i.pinimg.com/originals/9c/da/64/9cda6405c91f0a6060592638e58932c4.gif",
+              },
+            }}
+          />
+        </Button>
+      </DialogTrigger>
+      <DialogSurface style={{ marginRight: "20px", marginBottom: "20px" }}>
+        <DialogBody>
+          <DialogTitle>Speak to CS·AI</DialogTitle>
+          <DialogContent>
+            <div>
+              <Subtitle2>Chat</Subtitle2>
+            </div>
+            <div style={{ textAlign: "center" }}>
+              <Text align="center" size={200}>
+                {new Date().toLocaleTimeString()}
+              </Text>
+            </div>
+            <div style={{ textAlign: "right" }}>
+              <Text>
+                This is an example of the most recent ChatGPT Question.{" "}
+              </Text>
+            </div>
+            <br />
+            <div style={{ textAlign: "left" }}>
+              <Text font="monospace">
+                This is an example of the most recent ChatGPT Reply.
+              </Text>
+            </div>
+            <br />
+            <div style={{ width: "100%" }}>
+              <Input
+                appearance="underline"
+                contentAfter={
+                  <Button appearance="transparent" icon={<SendRegular />} />
+                }
+                id={underlineId}
+                style={{ width: "100%" }}
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <DialogTrigger disableButtonEnhancement>
+              <Button appearance="secondary">Close</Button>
+            </DialogTrigger>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
+  );
+};
