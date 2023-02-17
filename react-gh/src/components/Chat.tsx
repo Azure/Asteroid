0efea6db28d3b27830e61439af65918d39cc8a35
70mm1 <mail@tmirwald.de>
2023-02-17 11:34:54 +0100

    Improved Chat Design Mockup

--- react-gh/src/components/Chat.tsx
+++ react-gh/src/components/Chat.tsx
@@ -7,75 +7,125 @@ import {
   DialogBody,
   Input,
   useId,
-  Text,
   Subtitle2,
+  Body2,
+  tokens,
   Button,
   FluentProvider,
-  Persona
+  Persona,
+  makeStyles,
+  shorthands,
 } from "@fluentui/react-components";
 
 import * as React from "react";
+import { useState } from "react";
 
 import { SendRegular } from "@fluentui/react-icons";
 
-export const Chat: React.FunctionComponent = ({}) => {
+const useStyles = makeStyles({
+  userChatBubble: {
+    backgroundColor: tokens.colorBrandForegroundInverted,
+    ...shorthands.borderRadius(tokens.borderRadiusLarge),
+    ...shorthands.padding("8px"),
+  },
+  aiChatBubble: {
+    backgroundColor: tokens.colorBrandForegroundInverted,
+    ...shorthands.borderRadius(tokens.borderRadiusLarge),
+    ...shorthands.padding("8px"),
+    inlineSize: "100px",
+    overflowWrap: "break-word",
+  },
+  generalChatBubble: {},
+});
+
+export const Chat: React.FunctionComponent = () => {
+  const styling = useStyles();
+
+  let [message, setMesssage] = useState<string>("");
+  let [chatContent, setChatContent] = useState<string[]>([]);
+
   const underlineId = useId("input-underline");
+
   return (
     <FluentProvider>
-    <Dialog modalType="non-modal">
-      <DialogTrigger disableButtonEnhancement>
-        <Button appearance="transparent">
-          <Persona
-            name="CS·AI Bot"
-            size="huge"
-            secondaryText="Ask Azure Questions"
-            presence={{ status: "available" }}
-            avatar={{
-              image: {
-                src: "https://i.pinimg.com/originals/9c/da/64/9cda6405c91f0a6060592638e58932c4.gif",
-              },
-            }}
-          />
-        </Button>
-      </DialogTrigger>
-      <DialogSurface style={{ marginRight: "20px", marginBottom: "20px" }}>
-        <DialogBody>
-          <DialogTitle>Speak to CS·AI</DialogTitle>
-          <DialogContent>
-            <div>
-              <Subtitle2>Chat</Subtitle2>
-            </div>
-            <div style={{ textAlign: "center" }}>
-              <Text align="center" size={200}>
-                {new Date().toLocaleTimeString()}
-              </Text>
-            </div>
-            <div style={{ textAlign: "right" }}>
-              <Text>
-                This is an example of the most recent ChatGPT Question.{" "}
-              </Text>
-            </div>
-            <br />
-            <div style={{ textAlign: "left" }}>
-              <Text font="monospace">
-                This is an example of the most recent ChatGPT Reply.
-              </Text>
-            </div>
-            <br />
-            <div style={{ width: "100%" }}>
-              <Input
-                appearance="underline"
-                contentAfter={
-                  <Button appearance="transparent" icon={<SendRegular />} />
-                }
-                id={underlineId}
-                style={{ width: "100%" }}
-              />
-            </div>
-          </DialogContent>
-        </DialogBody>
-      </DialogSurface>
-    </Dialog>
+      <Dialog modalType="non-modal">
+        <DialogTrigger disableButtonEnhancement>
+          <Button appearance="transparent">
+            <Persona
+              name="CS·AI Bot"
+              size="huge"
+              secondaryText="Ask Azure Questions"
+              presence={{ status: "available" }}
+              avatar={{
+                image: {
+                  src: "https://i.pinimg.com/originals/9c/da/64/9cda6405c91f0a6060592638e58932c4.gif",
+                },
+              }}
+            />
+          </Button>
+        </DialogTrigger>
+        <DialogSurface style={{ marginRight: "20px", marginBottom: "20px" }}>
+          <DialogBody>
+            <DialogTitle>Speak to CS·AI</DialogTitle>
+            <DialogContent>
+              <div>
+                <Subtitle2>Chat</Subtitle2>
+              </div>
+              <div>
+                {chatContent.map((message, index) =>
+                  index % 2 === 0 ? (
+                    <div>
+                      <div>
+                        <br />
+                      </div>
+                      <div
+                        className={styling.generalChatBubble}
+                        style={{ textAlign: "right" }}
+                      >
+                        <Body2 className={styling.userChatBubble}>
+                          {message}
+                        </Body2>{" "}
+                      </div>
+                      <div>
+                        <br />
+                      </div>
+                    </div>
+                  ) : (
+                    <div style={{ textAlign: "left" }}>
+                      <Body2 className={styling.aiChatBubble}>{message}</Body2>{" "}
+                      <br />
+                    </div>
+                  )
+                )}
+              </div>
+              <br />
+              <div style={{ width: "100%" }}>
+                <Input
+                  appearance="underline"
+                  contentAfter={
+                    <Button
+                      appearance="transparent"
+                      icon={<SendRegular />}
+                      onClick={() => {
+                        setMesssage("");
+                        setChatContent([
+                          ...chatContent,
+                          message,
+                          "This feature is not yet implemented",
+                        ]);
+                      }}
+                    />
+                  }
+                  id={underlineId}
+                  style={{ width: "100%" }}
+                  value={message}
+                  onChange={(e, data) => setMesssage(data.value)}
+                />
+              </div>
+            </DialogContent>
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>
     </FluentProvider>
   );
 };
